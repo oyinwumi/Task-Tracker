98f73418705adc301f92f53bb57345f725700edf
Oyindamola Ogunknle <oyinwumi@gmail.com>
2022-11-12 01:43:23 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import "./App.css"
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    }
+])
+
+//delete task
+const deleteTask = (id) => {
+ setTasks(tasks.filter((task) =>task.id !== id))
+}
+
+  return (
+    <div className="container">
+      <Header/>
+   { tasks.length> 0 ?  <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Task to show'}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import  Button from "./Button"
+
+const Header = () => {
+    const onClick = () =>{
+        console.log("click")
+    }
+
+  return (
+    <header className='header'>
+      <h1>Task Tracker</h1>
+      <Button color = "green" text= "Add" onClick ={onClick}/>
+    </header>
+  );
+}
+
+
+
+export default Header;
