0483b05b42c0d29dbbfb0f32b217a1c3f6311daf
Oyindamola Ogunknle <oyinwumi@gmail.com>
2022-11-15 00:05:56 +0100

    contination

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from './components/AddTask';
 import "./App.css"
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -25,15 +27,38 @@ const App = () => {
     }
 ])
 
+//Add Task
+const addTask = (task) =>{
+  const id = Math.floor(Math.random()*10000)+1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
+
+
 //delete task
 const deleteTask = (id) => {
  setTasks(tasks.filter((task) =>task.id !== id))
 }
 
+//Toggle reminder
+const toggleReminder = (id) =>{
+  setTasks(tasks.map((task) =>task.id === id 
+  ?{...task, reminder: !task.reminder} :task))
+}
+
   return (
     <div className="container">
-      <Header/>
-   { tasks.length> 0 ?  <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Task to show'}
+      <Header 
+      onAdd={() =>setShowAddTask
+      (!showAddTask)}
+       showAdd={showAddTask}
+       />
+
+       { showAddTask && < AddTask onAdd={addTask}/> }  
+      { tasks.length> 0 ?  
+      <Tasks tasks={tasks} onDelete=
+      {deleteTask} onToggle = {toggleReminder} /> : 
+   'No Task to show'}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import  Button from "./Button"
 
-const Header = () => {
+const Header = ({title,OnAdd},showAddTask) => {
     const onClick = () =>{
         console.log("click")
     }
 
   return (
     <header className='header'>
-      <h1>Task Tracker</h1>
+      <h1>{title}</h1>
       <Button color = "green" text= "Add" onClick ={onClick}/>
     </header>
   );
 }
 
-
-
+Header.defaultProps = {
+  title:"Task Tracker",
+}
+Header.propTypes ={
+  title: PropTypes.string.isRequired,
+}
 export default Header;
